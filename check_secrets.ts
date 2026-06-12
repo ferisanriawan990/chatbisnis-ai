0eb57b0ada54e2c2d9a7ec97b423c481d198b6a7
Macbook Pro <mymac@MacBook.local>
2026-06-12 19:23:27 +0700

    chore: clean up root directory debugging scripts

--- check_secrets.ts
+++
@@ -1,7 +0,0 @@
-import { prisma } from './src/lib/prisma.js';
-
-async function check() {
-  const secrets = await prisma.secretCredential.findMany();
-  console.log('All secrets:', secrets.map(s => s.key));
-}
-check().catch(console.error).finally(() => process.exit(0));
