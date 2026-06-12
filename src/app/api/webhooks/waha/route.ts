5160796abdd3cf7f68ea2b049175e5529489e513
Macbook Pro <mymac@MacBook.local>
2026-06-12 17:23:27 +0700

    fix: ignore admin@chatbisnis.id in webhook query

--- src/app/api/webhooks/waha/route.ts
+++ src/app/api/webhooks/waha/route.ts
@@ -173,6 +173,7 @@ export async function POST(req: NextRequest) {
         ...(qUserId && process.env.NODE_ENV !== 'production' ? { userId: qUserId } : {}),
         isActive: true,
         user: {
+          email: { not: 'admin@chatbisnis.id' },
           subscriptions: {
             some: { status: 'active' }
           }
